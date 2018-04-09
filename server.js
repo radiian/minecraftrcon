659b15f4b23686b76412f79efb957b7fdbe30f07
Zech <thompzac@oregonstate.edu>
2018-04-08 20:07:02 -0700

    Working basic prototype that can log players to influxdb

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const net = require('net');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+const http = require('http');
 
 console.log("started server");
+
 var host = '127.0.0.1';
 var port = 25575;
-
+var gid = 1;
 var connected = false;
-	
+
+
+var cmdqueue = [];
+cmdqueue[0] = "0";//beginning of queue
+
 var client = new net.Socket();
 client.setKeepAlive(true);
 client.setEncoding("ASCII");	
@@ -28,13 +28,62 @@ function pack(id, type, message){
 	buff.writeInt16LE(0, (12+message.length));
 	return buff;
 }
-		
-	var test = pack(10, 3, "mcarc1234$");
-	//console.log(test);
-	client.write(test);
 
-	var pack2 = pack(11, 2, "list");
-	client.write(pack2);
+function sendcmd(command){
+	if(!connected)return;
+	if(gid > 2000) gid = 0;//make sure the id doesnt get too big
+	++gid;
+	cmdqueue[gid] = command;
+	var cmdpack = pack(gid, 2, command);
+	client.write(cmdpack);
+}
+
+function login(password){
+	if(connected) return;
+	++gid;
+	cmdqueue[gid] = "login";
+	var logpack = pack(gid, 3, password);
+	client.write(logpack);	
+}
+
+function getcount(){
+	sendcmd("list");
+}
+
+function announce(){
+	sendcmd("say Hello peoples!");
+}
+
+function influxIt(measurement, value){
+	console.log("writing measurement: " + measurement + " with value: " + value);
+	var data = measurement + " value=" + value;
+	var opt = {
+		host: "10.0.0.21",
+		port: 8086,
+		path: '/write?db=telegraf',// + db,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(data)
+		}
+	};
+	const req = http.request(opt, (res) => {});
+	req.write(data);
+	req.end();
+}
+
+	
+	login("mcarc1234$");
+	setInterval(getcount, 2000);
+	//setInterval(announce, 5000);
+	//while(!connected){}
+	//sendcmd("list");	
+	//var test = pack(10, 3, "mcarc1234$");
+	//console.log(test);
+	//client.write(test);
+
+	//var pack2 = pack(11, 2, "list");
+	//client.write(pack2);
 	//var help = pack(12, 2, "help");
 	//client.write(help);
 	//client.write(finalmessage.toString("utf8"), "UTF8", function(){
@@ -43,9 +92,39 @@ function pack(id, type, message){
 		//});
 
 client.on('data', function(data){
-	console.log("got data: " + data);
+	console.log("got data");
 	var dbuff = Buffer.from(data);
-	console.log(dbuff);
+	//console.log(dbuff);
+	var length = dbuff.readInt32LE(0);
+	var id = dbuff.readInt32LE(4);
+	var type = dbuff.readInt32LE(8);
+	var messagebuff = dbuff.slice(12, length+4);
+	console.log("Length: " + length);
+	console.log("Buff size: " + dbuff.length);
+	console.log("id: " + id);
+	console.log("type: " + type);
+	console.log(messagebuff.toString('ascii'));
+	var response = messagebuff.toString('ascii');
+	var sentcmd = "";
+	sentcmd = cmdqueue[id];
+	if(sentcmd == "list"){
+		//log it to the thing
+		console.log("Going to write data to influx");		
+		console.log(response);
+		var rgx = response.match("[0-9]+\/[0-9]+");
+		if(rgx != null){
+			//console.log(rgx[0]);
+			var substr = rgx[0].split("/");
+			influxIt("players", substr[0]);
+			influxIt("maxplayers", substr[1]);
+		}
+	}
+	console.log("response to cmd: " + sentcmd);
+	if(id == -1)console.log("Failed to log in");
+	if(id == gid && type == 2){
+		console.log("Logged in ok");
+		connected = true;
+	}
 });
 
 client.on("close", function(){
